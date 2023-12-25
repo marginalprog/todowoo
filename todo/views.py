201806c86b6264b817e6fb5633e50fb1c6e79622
Ivan <korkishko.v@bk.ru>
2023-12-25 18:10:45 +0300

    Реализован вход и выход пользователей

--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, login, authenticate
 
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'todo/home.html')
+
+
 def signupuser(request):
     if request.method == 'GET':
         return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
@@ -26,5 +30,23 @@ def signupuser(request):
             # Уведомление о неправильном пароле
 
 
+def loginuser(request):
+    if request.method == 'GET': # Если приходит гет-запрос, то перенаправляем пользователя на логин
+        return render(request, 'todo/loginuser.html', {'form': AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todo/loginuser.html', {'form': AuthenticationForm(), 'error':'Некорректный пароль пользователя'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')  # !!! Перенаправление на ниже созданное отображение
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+
 def currenttodos(request):
     return render(request, 'todo/currenttodos.html')
+
+
--- todowoo/urls.py
+++ todowoo/urls.py
@@ -21,10 +21,16 @@ from todo import views
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    path('', views.home, name='home'),
+
     # auth
     path('signup/', views.signupuser, name='signupuser'),
+    path('logout/', views.logoutuser, name='logoutuser'),
+    path('login/', views.loginuser, name='loginuser'),
 
     #todos
     path('current/', views.currenttodos, name='currenttodos'),
+
+
 
 ]
