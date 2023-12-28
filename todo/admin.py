8a278e0d870a541994fbcbcec9d5b29b6e62764e
Ivan <korkishko.v@bk.ru>
2023-12-28 21:24:34 +0300

    Создана модель для работы с делами\планами пользователей

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Todo
 
-# Register your models here.
+
+class TodoAdmin(admin.ModelAdmin):
+    # ModelAdmin используется для кастомизации интерфейса администратора
+    # Для отображения даты создания записи
+    readonly_fields = ('date_created',)
+
+
+admin.site.register(Todo, TodoAdmin)
